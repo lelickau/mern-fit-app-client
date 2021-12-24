c4aae52aa5044dc009ad514533e27396b2afdca7
elena_kau <lelickau@yandex.ru>
2021-12-24 16:49:34 +0200

    test 1

---
+++ src/env.js
@@ -0,0 +1,15 @@
+// API FOOD
+export const APP_ID = 'a2a54800';
+export const API_KEY = 'afc67ff1d7b58baa75dfdfe65774d387';
+export const API_FOOD_URL = `https://api.edamam.com/api/food-database/v2/parser?app_id=${APP_ID}&app_key=${API_KEY}`;
+
+// MY API
+export const SERVER_URL = `https://fit-app-mern.herokuapp.com/`;
+export const API_URL_AUTH = `https://fit-app-mern.herokuapp.com/api/auth/`;
+export const MY_API_URL_FOODS = `https://fit-app-mern.herokuapp.com/api/foods/`;
+export const API_URL_NOTES = `https://fit-app-mern.herokuapp.com/api/notes/`;
+
+
+
+
+
---
+++ src/http/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { API_URL_AUTH } from '../env';
+
+const $api = axios.create({
+    withCredentials: true,
+    baseURL: API_URL_AUTH
+});
+
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    return config;
+});
+
+$api.interceptors.response.use((config) => {
+    return config;
+}, async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get(`${API_URL_AUTH}refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken);
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.log('The user is not logged in')
+        }
+    }
+    throw error;
+})
+
+export default $api;
