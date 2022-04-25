fa95a7ad38d8c6acec6e1387f78fa216a3306d3f
elena_kau <lelickau@yandex.ru>
2022-04-25 19:18:33 +0200

    avatar delete

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -4,28 +4,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { deleteAvatar, uploadAvatar } from '../../redux/actions/user';
 import {SERVER_URL} from '../../env.js';
 
-import editBtn from '../../resources/icons/edit-pen.svg';
 import defaultAvatar from '../../resources/img/profile-img.png';
 import './homePage.scss';
 
 function HomePage() {
     const user = useSelector(state => state.user.currentUser);
     const avatar = user.avatar ? `${SERVER_URL + user.avatar}` : defaultAvatar;
-    const dispatch = useDispatch();
 
-    const changeHandler = (e) => {
-        if (user.avatar) {
-            dispatch(deleteAvatar());
-        }
-        const file = e.target.files[0];
-        dispatch(uploadAvatar(file));
-    }
     return (
         <div className="profile">
             <HeaderTitle>Profile</HeaderTitle>
             <article className="profile__content container">
                 <div className="profile__box">
-
                     <div className="profile__ava-box">
                         <img className="profile__ava" src={avatar} alt="User" />
                     </div>
